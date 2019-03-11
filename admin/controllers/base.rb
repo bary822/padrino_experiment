2a547fb98801a7c8cdc7d22a950ab70745e55630
bary822 <hiroto.fukui822@gmail.com>
2019-03-11 18:13:19 +0900

    bundle exec padrino g admin

---
+++ admin/controllers/base.rb
@@ -0,0 +1,5 @@
+PadrinoExperiment::Admin.controllers :base do
+  get :index, :map => "/" do
+    render "base/index"
+  end
+end
--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres://localhost/padrino_experiment_development", :loggers => [logger])
+  when :development then Sequel.connect("postgres://#{ENV['db_url']}/padrino_experiment_development", :loggers => [logger])
   when :production  then Sequel.connect("postgres://localhost/padrino_experiment_production",  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/padrino_experiment_test",        :loggers => [logger])
 end

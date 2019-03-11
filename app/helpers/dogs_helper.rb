2b7d39e1bc0260269a42583a4160a0113e1a52c7
bary822 <hiroto.fukui822@gmail.com>
2019-03-11 16:57:27 +0900

    bundle exec padrino g controller dogs

---
+++ app/helpers/dogs_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module PadrinoExperiment
+  class App
+    module DogsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers DogsHelper
+  end
+end
